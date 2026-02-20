337e962be19ed0a5085143d7610dfacfe822ae6c
Aryan <aryanrodwal@gamil.com>
2026-02-20 21:37:14 +0530

    add sub rect app

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css'
+import { useState } from 'react';
+
+function App() {
+  // let Counter = 15;
+
+  let [Counter,setCounter]= useState(15);
+
+  const addValue = () =>{
+    console.log("clicked",Counter);
+    // Counter = Counter + 1;
+    setCounter(Counter+1);
+  }
+
+  const removeValue = () => {
+    console.log("clicked",Counter);
+    // another way to write
+    Counter=Counter - 1;
+    setCounter(Counter);
+  }
+
+  return (
+    <>
+    <h1>Aryan Yadav</h1>
+    <h2>Counter value : {Counter}</h2>
+
+    <button
+    onClick={addValue}
+    >Add Value</button>
+    <br />
+    <button
+    onClick={removeValue}
+    >Remove value</button>
+    </>
+  )
+}
+
+export default App
